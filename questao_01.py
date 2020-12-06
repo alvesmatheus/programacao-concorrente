7d47154b44908d2eda8a92d36e24090b982888e9
alvesmatheus <matheus.santos@ccc.ufcg.edu.br>
2020-12-06 20:44:33 -0300

    Adiciona comentários às questões 01 e 03

--- questao_01.py
+++ questao_01.py
@@ -1,65 +1,67 @@
 from threading import Thread, Semaphore
-import random, sys, time
+import random
+import sys
+import time
 
 
 class Car:
     def __init__(self, capacity):
-        self.seats = capacity
-        self.passengers = 0
+        self.seats = capacity  # Capacidade máxima do carro
+        self.passengers = 0  # Passageiros dentro do carro atualmente
 
-        self.loading = Semaphore(0)
-        self.unloading = Semaphore(0)
-        self.loaded = Semaphore(0)
-        self.unloaded = Semaphore(0)
-
+        self.loading = Semaphore(0)  # Os passageiros podem embarcar?
+        self.unloading = Semaphore(0)  # Os passageiros podem desembarcar?
+        self.loaded = Semaphore(0)  # Todos os passageiros embarcaram?
+        self.unloaded = Semaphore(0)  # Todos os passageiros desembarcaram?
 
     def is_full(self):
         return (self.passengers == self.seats)
 
-
     def is_empty(self):
         return (self.passengers == 0)
 
-
     def load(self):
         print('\nO carro está aguardando novos passageiros...\n')
-        time.sleep(1)
-        self.loading.release()
-
+        self.loading.release()  # Um passageiro pode embarcar agora!
 
     def run(self):
         print('\nO carro está em movimento...')
-        time.sleep(1)
         self.unload()
 
-
     def unload(self):
-        print('O carro está desembarcando passageiros...\n')
-        time.sleep(1)
-        self.unloading.release()
+        print('O carro está desembarcando seus passageiros...\n')
+        self.unloading.release()  # Um passageiro pode desembarcar agora!
 
 
 class Passenger:
     def __init__(self, identifier):
-        self.identifier = identifier
-        self.onboard = False
-
+        self.identifier = identifier  # Identificador único do passageiro
+        self.onboard = False  # Indica se o passageiro está dentro do carro
 
     def board(self, car):
         print(f'O passageiro {self.identifier} embarcou!')
         self.onboard = True
 
+        # Impede que outros passageiros embarquem simultaneamente, incrementa o
+        # número de passageiros e, só então, permite que outros passageiros
+        # embarquem. Antes disso, se a capacidade do carro for atingida, indica
+        # ao carro que todos os passageiros já embarcaram.
+        time.sleep(random.random())  # Espera randômica para embarcar
         car.loading.acquire()
         car.passengers += 1
         if car.is_full():
             car.loaded.release()
         car.loading.release()
 
-
     def unboard(self, car):
         print(f'O passageiro {self.identifier} desembarcou!')
         self.onboard = False
 
+        # Impede que outros passageiros desembarquem simultanemante, decrementa
+        # o número de passageiros e, só então, permite que outros passageiros
+        # desembarquem. Antes disso, se não houver mais passageiros no carro,
+        # indica ao carro que todos os passageiros já desembarcaram.
+        time.sleep(random.random())  # Espera randômica para desembarcar
         car.unloading.acquire()
         car.passengers -= 1
         if car.is_empty():
@@ -68,6 +70,14 @@ class Passenger:
 
 
 def run_car(car, capacity, n_passengers, board_queue, unboard_queue):
+    # Inicialmente, o carro está aguardando novos passageiros. Ele permanecerá
+    # assim até que sua capacidade seja atingida, o que ocorre quando o último
+    # passageiro possível entra no carro e indica isso através do `loaded`.
+    # Assim, carro entrará em movimento e passará a aguardar o desembarque dos
+    # passageiros. O último passageiro a desembarcar indica ao carro que ele
+    # está vazio através do `unloaded`. O processo se repetirá de acordo com o
+    # o número de viagens que o carro poderá realizar com o os passageiros
+    # existentes.
     for _ in range(n_passengers // capacity):
         car.load()
         [board_queue.release() for _ in range(capacity)]
@@ -79,22 +89,27 @@ def run_car(car, capacity, n_passengers, board_queue, unboard_queue):
 
 
 def run_passenger(passenger, car, board_queue, unboard_queue):
-    time.sleep(random.random())
+    # Um passageiro aguardará na fila até que possa embarcar no carro, o que
+    # só ocorrerá se o carro estiver aguardando novos passageiros e nenhum
+    # outro passageiro estiver embarcando simultaneamente. De forma análoga,
+    # quando o carro permitir o desembarque dos passageiros que embarcaram
+    # o passageiro não desembarcará a menos que nenhum outro passageiro esteja
+    # desembarcando ao mesmo tempo.
     board_queue.acquire()
     passenger.board(car)
 
-    time.sleep(random.random())
     unboard_queue.acquire()
     passenger.unboard(car)
 
 
 def main(n_passengers, car_capacity):
-    board_queue = Semaphore(0)
-    unboard_queue = Semaphore(0)
+    board_queue = Semaphore(0)  # Quantos passageiros irão embarcar?
+    unboard_queue = Semaphore(0)  # Quantos passageiros irão desembarcar?
     kwargs = {'board_queue': board_queue, 'unboard_queue': unboard_queue}
 
     car = Car(car_capacity)
     passengers = [Passenger(i + 1) for i in range(n_passengers)]
+    random.shuffle(passengers)  # Randomiza a ordem de início das threads
 
     car_thread = Thread(target=run_car,
                         args=[car, car_capacity, n_passengers],
--- questao_03.py
+++ questao_03.py
@@ -1,25 +1,32 @@
 from threading import Thread, Semaphore
-import random, sys, time
+import random
+import sys
+import time
 
 
 class BarTable:
     def __init__(self):
-        self.customers = 0
-        self.leavers = 0
+        self.customers = 0  # Número de alunos que estão bebendo
+        self.leavers = 0  # Número de alunos que estão remedidados
 
-        self.status_changing = Semaphore(1)
-        self.allowed_leaving = Semaphore(0)
+        self.status_changing = Semaphore(1)  # Alunos podem mudar de estado?
+        self.allowed_leaving = Semaphore(0)  # Alunos podem ir embora?
 
 
 class Student:
     def __init__(self, identifier):
-        self.identifier = identifier
-
+        self.identifier = identifier  # Identificador único do aluno
 
     def drink(self, table):
         print(f'O aluno {self.identifier} chegou e pediu uma cerveja...')
-        time.sleep(random.random())
+        time.sleep(random.random())  # Espera randômica para começar a beber
 
+        # Quando um aluno chega ao bar, ele pede sua bebida. Se já havia um
+        # aluno A remediado que não podia ir embora porque um aluno B ainda
+        # estava bebendo, o aluno A irá embora. A seguir, o aluno que chegou
+        # começa e beber e, quando termina, muda de estado para remediado se,
+        # e somente se, nenhum outro aluno estiver mudando de estado ao mesmo
+        # tempo.
         table.status_changing.acquire()
         table.customers += 1
         if (table.customers == 2) and (table.leavers == 1):
@@ -28,15 +35,20 @@ class Student:
         table.status_changing.release()
 
         print(f'O aluno {self.identifier} começou a beber!')
-        time.sleep(0.5)
+        time.sleep(random.random())  # O aluno bebe durante um tempo randômico
 
         table.status_changing.acquire()
         print(f'O aluno {self.identifier} está esperando para ir embora...')
         table.customers -= 1
         table.leavers += 1
 
-
     def leave(self, table):
+        # Quando um aluno deseja ir embora, primeiro ele deverá checar se na
+        # mesa resta apenas um aluno além dele. Se isso ocorrer e o aluno ainda
+        # está bebendo, ele ficara aguardando a permissão para ir embora. Já
+        # se o outro aluno á estava aguardando, ambos irão embora juntos. Por
+        # fim, se há mais de um aluno na mesa quando o aluno decidiu ir embora,
+        # ele não precisará se preocupar com eles e apenas irá embora.
         if (table.customers == 1) and (table.leavers == 1):
             table.status_changing.release()
             table.allowed_leaving.acquire()
@@ -54,6 +66,9 @@ class Student:
 
 
 def run_student(student, bar_table):
+    # O comportamento do aluno consiste apenas em ir até o bar, beber e ir
+    # embora. As restrições impostas a esses comportamento estão descritas na
+    # classe Student.
     student.drink(bar_table)
     student.leave(bar_table)
 
@@ -61,6 +76,7 @@ def run_student(student, bar_table):
 def main(n_students):
     bar_table = BarTable()
     students = [Student(i + 1) for i in range(n_students)]
+    random.shuffle(students)  # Randomiza a ordem de início das threads
 
     student_threads = [
         Thread(target=run_student, args=[student, bar_table])
